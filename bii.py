799facab192e42a5443eb6af38260121af8a2f35
Luis Martínez de Bartolomé Izquierdo <lasote@gmail.com>
2015-04-15 10:25:59 +0200

    Fixed bii script

--- bii.py
+++ bii.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
+
 import sys, os
-sys.path.append(os.path.join(os.getcwd(), "../"))
+
+biicode_repo_path = "" # ABSOLUTE PATH TO BIICODE REPOSITORY FOLDER
+
+sys.path.append(os.path.join(biicode_repo_path, "../"))
 from biicode.client.shell.bii import main
 main(sys.argv[1:])
